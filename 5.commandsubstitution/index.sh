f7f1d3bbca1eb3eb847b28e18e8add7ecff4b1b9
Prahalad Belavadi <PrahaladBelavadi@gmail.com>
2018-06-28 13:08:27 +0530

    list wordcount in working directory

--- 5.commandsubstitution/index.sh
+++ 5.commandsubstitution/index.sh
@@ -2,3 +2,7 @@
 
 myvar=$( ls /etc | wc -l )
 echo There are $myvar entries in the directory /etc
+
+mynewvar=$( ls ~/Desktop | wc -l )
+workingdr=$( pwd )
+echo There are $mynewvar files in $workingdr
