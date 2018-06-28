683fe94d785eb32244e148b0d8803d710cac28f4
Prahalad Belavadi <PrahaladBelavadi@gmail.com>
2018-06-28 14:29:58 +0530

    inut

---
+++ 7.takingInput/login.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+read -p 'UserName: ' uservar
+read -sp 'Password: ' passvar
+
+echo
+
+echo Thank You $uservar , we now have your login details
+
+echo You just entered
+echo "Username: $uservar"
+echo "Password: $passvar"
