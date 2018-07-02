56fc2813cc5c09f9649cc31ee66119854399f2b8
Prahalad Belavadi <PrahaladBelavadi@gmail.com>
2018-07-02 16:10:06 +0530

    old chaanges

---
+++ 10.expresssions/double_parantheses.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+a=$(( 4 + 5 ))
+echo $a
+
+a=$((3+5))
+echo $a
+
+b=$(( a + 3 ))
+echo $b
+
+b=$(( $a + 4 ))
+echo $b
+
+(( b++ ))
+echo $b
+
+(( b += 3 ))
+echo $b
+
+a=$(( 4*5 ))
+echo $a
---
+++ 10.expresssions/length_example.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+a='Hello World'
+echo ${#a}
+
+b=4953
+echo ${#b}
+
+c=$0
+echo ${#c}
